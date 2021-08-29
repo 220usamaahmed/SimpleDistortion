c5db77619d742eb3fbdec8f2ebc9579c3b4febe9
Usama Ahmed <220usamaahmed@gmail.com>
2021-08-30 00:05:50 +0500

    Aesthetic changes

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -16,19 +16,19 @@ SimpleDistortionAudioProcessorEditor::SimpleDistortionAudioProcessorEditor (Simp
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     driveSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
-    driveSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 96, 48);
+    driveSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 80, 16);
     addAndMakeVisible(driveSlider);
 
     rangeSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
-    rangeSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 96, 48);
+    rangeSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 80, 16);
     addAndMakeVisible(rangeSlider);
 
     blendSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
-    blendSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 96, 48);
+    blendSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 80, 16);
     addAndMakeVisible(blendSlider);
 
     volumeSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
-    volumeSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 100, 50);
+    volumeSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 80, 16);
     addAndMakeVisible(volumeSlider);
 
     driveSliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, "Drive", driveSlider);
@@ -36,7 +36,7 @@ SimpleDistortionAudioProcessorEditor::SimpleDistortionAudioProcessorEditor (Simp
     blendSliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, "Blend", blendSlider);
     volumeSliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, "Volume", volumeSlider);
 
-    setSize (400, 200);
+    setSize (400, 160);
 }
 
 SimpleDistortionAudioProcessorEditor::~SimpleDistortionAudioProcessorEditor()
@@ -48,18 +48,14 @@ void SimpleDistortionAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void SimpleDistortionAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
-    driveSlider.setBounds(10, getHeight() / 2 - 40, 80, 80);
-    rangeSlider.setBounds(110, getHeight() / 2 - 40, 80, 80);
-    blendSlider.setBounds(210, getHeight() / 2 - 40, 80, 80);
-    volumeSlider.setBounds(310, getHeight() / 2 - 40, 80, 80);
+    driveSlider.setBounds(0, getHeight() / 2 - 50, 100, 100);
+    rangeSlider.setBounds(100, getHeight() / 2 - 50, 100, 100);
+    blendSlider.setBounds(200, getHeight() / 2 - 50, 100, 100);
+    volumeSlider.setBounds(300, getHeight() / 2 - 50, 100, 100);
 }
